2e907c0eb6797fb3c75d519907f48129e42b51a1
Emile Moraes <moraesemile@gmail.com>
2022-11-15 20:34:57 -0300

    Projects created

---
+++ backend/src/main/java/com/emissales/emissales/EmisSalesApplication.java
@@ -0,0 +1,13 @@
+package com.emissales.emissales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmisSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmisSalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/emissales/emissales/EmisSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emissales.emissales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmisSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
