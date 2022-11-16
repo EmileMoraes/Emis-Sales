9cc80962c86236b9c3847be7d1b6ed2b4b806a1e
Emile Moraes <moraesemile@gmail.com>
2022-11-15 22:54:02 -0300

    First component button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/notification-button"
 
 function App() {
-  return(<h1>Welcome</h1>)
+  return(
+  <>
+  <h1>Welcome</h1>
+  <NotificationButton/>
+  </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/notification-button/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/Vector.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="emisales-red-botton">
+            <img src={icon} alt="notification" />
+        </div>
+    )
+}
+
+export default NotificationButton
