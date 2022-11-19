01a420c2679809c0ccf1f008a46d10b28d1845f5
Emile Moraes <moraesemile@gmail.com>
2022-11-18 23:38:18 -0300

    Create fronted sales

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/Vector.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClik(id: number) {
+    axios(`${BASE_URL}/v0/api/sales/${id}/notification`)
+    .then(response => {
+        toast.info("SMS send successfully!")
+    })
+}
+
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="emisales-red-botton">
+        <div className="emisales-red-botton" onClick={() => handleClik(saleId)}>
             <img src={icon} alt="notification" />
         </div>
     )
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,12 +3,29 @@ import './styles.css';
 
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../../utils/request';
+import { Sale } from '../../models/sale';
 
 function SalesCard() {
 
+
     const [minDate, setMinDate] = useState(new Date());
     const [maxDate, setMaxDate] = useState(new Date());
+
+    const [sales, setSales] = useState<Sale[]>([]);
+
+    useEffect(() => {
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/v0/api/sales/date?minDate=${dmin}&maxDate=${dmax}`)
+            .then(response => {
+                setSales(response.data.content);
+            });
+    }, [minDate, maxDate]);
 
     return (
         <div className="emisales-card">
@@ -47,45 +64,25 @@ function SalesCard() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td className="show992">007</td>
-                            <td className="show576">11/14/2022</td>
-                            <td>Julia</td>
-                            <td className="show992">15</td>
-                            <td className="show992">2000</td>
-                            <td>USD 33 0000</td>
-                            <td>
-                                <div className="emisales-red-botton-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">001</td>
-                            <td className="show576">10/14/2022</td>
-                            <td>Citta</td>
-                            <td className="show992">15</td>
-                            <td className="show992">2000</td>
-                            <td>USD 10000</td>
-                            <td>
-                                <div className="emisales-red-botton-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">005</td>
-                            <td className="show576">02/14/2022</td>
-                            <td>Herman</td>
-                            <td className="show992">15</td>
-                            <td className="show992">2000</td>
-                            <td>USD 22 0000</td>
-                            <td>
-                                <div className="emisales-red-botton-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
+                        {
+                            sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>{sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="emisales-red-botton-container">
+                                                <NotificationButton saleId={sale.id}/>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
